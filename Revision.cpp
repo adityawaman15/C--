be13b1f1f7ceb477192c5eb9a6aac97b79138ac4
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2025-12-22 17:44:48 +0000

    Dec 22, 2025, 11:14 PM

--- Revision.cpp
+++ Revision.cpp
@@ -2,10 +2,25 @@
 using namespace std;
 
 int main(){
-    int a= 1;
-    while (a <10){
-        cout << "a | "<< a<< endl;
-        
-        a +=1;
+    int num;
+    int a = 1;
+    int b = 1;
+    int c = 1;
+
+    cout << "Enter a number | ";
+    cin >> num;
+ while(c<= num){
+  while(b<=num){
+    while (a<=num){
+        cout << "*";
+        a+=1;
     }
+    cout << endl;
+      b+=1;
+      a=1;}
+      cout << endl;
+      b = 1;
+      c+=1;
+    num -=1;}
+    
 }
