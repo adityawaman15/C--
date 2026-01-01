93fe63c3d4d38f2520c30813086cf41c3a54cffa
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-01-01 13:07:56 +0000

    Jan 1, 2026, 6:37 PM

--- Revision.cpp
+++ Revision.cpp
@@ -2,19 +2,15 @@
 using namespace std;
 
 int main(){
-  cout << "Enter | ";
-  int a ;
-  int temp = a;
-  int b = a;
-  cin >> a;
+  int a;
 
-  while (b>0){
-    temp = a;
-    while (temp < b){
-    cout << temp << " ";
-    temp +=1;}
-    cout << endl;
-    a-=1;
-    b-=1;
+  cout << "Enter | ";
+  cin >> a;
+  int temp = 0;
+
+  while ( a > temp ){
+    cout << temp << endl;
+    temp +=1;
   }
+
 }
