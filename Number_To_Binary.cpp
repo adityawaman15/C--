e69122d23bf3758edc2bd4a186377f5f860b7e1d
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-01-01 17:46:41 +0000

    Jan 1, 2026, 11:16 PM

--- Number_To_Binary.cpp
+++ Number_To_Binary.cpp
@@ -6,14 +6,14 @@ int main(){
 
     int a = 5;
     int ans = 0;
-    int count = 1;
+    int count = 0;
 
     while(a!=0){
 
         
         int bit = a&1;
 
-        ans = ans + (bit * count);
+        ans = ans + pow(2,count);
         a = a>>1;
 
         count *= 10;}        
--- Revision.cpp
+++ Revision.cpp
@@ -2,15 +2,20 @@
 using namespace std;
 
 int main(){
-  int a;
 
-  cout << "Enter | ";
-  cin >> a;
-  int temp = 0;
+    int ch = 5;
 
-  while ( a > temp ){
-    cout << temp << endl;
-    temp +=1;
-  }
+    // Switch
 
-}
+    switch (ch){
+
+        case 1: cout << "Number 1"<< endl;;
+
+        //Use break or the rest of the cases will get executed too
+    
+
+        case 2: cout << "Number 2" << endl;;
+
+        default: cout << "Default case " << endl;
+    }
+} 
