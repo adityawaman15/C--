658cb13cadfd01646b550f02ddcd4d0693404612
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-01-04 17:53:04 +0000

    Jan 4, 2026, 11:23 PM

--- Revision.cpp
+++ Revision.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 using namespace std;
 
+void printArray (int arr[],int size){
+
+    for(int i = 0; i <size; i++){
+        cout << "Index "<< i << " | " << arr[i]<< endl;
+    }
+    
+}
+
+void inputArray (int arr[],int size){
+
+    for(int i = 0; i <size; i++){
+        cout << "Index "<< i << " | " ;
+        cin >> arr[i];
+        cout << endl;
+    }
+    
+}
 int main(){
     int array[10] = {0};
     cout << array[1] << endl;
+    inputArray(array,10);
+    cout << "The Array you entered is :- "<< endl;
+    printArray(array,10);}
 
-    array = {1,2,3};
-    cout << array[1] << endl;
-    cout << array[2] << endl;
-    cout << array[3] << endl;
-}
+
