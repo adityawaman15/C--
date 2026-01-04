493206519adb5d6500c75029cb386ff7d7c5cb71
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-01-04 17:40:56 +0000

    Jan 4, 2026, 11:10 PM

--- Revision.cpp
+++ Revision.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int pow(int a, int b){
+    int ans = 1;
+    if (b==0){
+        return 1;
+    }
+    for (int i = 0 ; i < b; i++){
+
+      ans *= a;
+        
+    }
+    return ans;
+}
 int main(){
 
-    int ch = 5;
+    int answer = pow(3,4);
+    cout << answer;
 
-    // Switch
 
-    switch (ch){
-
-        case 1: cout << "Number 1"<< endl;;
-
-        //Use break or the rest of the cases will get executed too
-    
-
-        case 2: cout << "Number 2" << endl;;
-
-        default: cout << "Default case " << endl;
-    }
-} 
+}
