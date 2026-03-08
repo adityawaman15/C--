bfaabccfaf01125de2da1629471a2b6109eda28e
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-03-08 09:57:39 +0000

    Mar 8, 2026, 3:27 PM

---
+++ Recursion_power2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int power(int num, int pow){
+    if(pow == 0){
+        return 1;
+    }
+
+    return num*power(num,pow-1);
+
+}
+
+int main(){
+    int ans = power(4,4);
+    cout << ans << endl;
+}
