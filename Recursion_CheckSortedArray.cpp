44cca76009c23a0d90fa35947e3655c8e42e1795
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-03-08 10:50:48 +0000

    Mar 8, 2026, 4:20 PM

--- Recursion_CheckSortedArray.cpp
+++ Recursion_CheckSortedArray.cpp
@@ -4,16 +4,20 @@ using namespace std;
 bool sort(int arr[], int size){
 
     if(size == 0){
-        return ans;
+        return true;
     }
 
-    bool ans = return arr[size-1]<arr[size] ? 1:0;
-    sort(arr,size-1);
+    bool ans = arr[size-1]<arr[size] ? 1:0;
+    if(ans == 0){
+        return false;
+    }
+    return sort(arr,size-1);
+    
 
 }
 
 int main(){
-    int arr[9] = {8,9,23,24,46,56,76,87,89};
+    int arr[9] = {8,9,23,24,46,56,76,87,86};
     int ans = sort(arr,8);
     cout << ans << endl;
 
