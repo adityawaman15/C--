ac15f547236162d60c600fd05bc67cb3c55f9b88
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-03-09 13:55:02 +0000

    Mar 9, 2026, 7:25 PM

---
+++ Recursion_Spelling_Numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void say(int n){
+    string arr[10] = {"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
+    cout << arr[n]<< endl;
+}
+
+void spell(){
+    if(n==0){
+        return 
+    }
+
+}
