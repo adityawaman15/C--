54a0622a928fad62558eab36770f8c3ae62a7bfe
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-03-13 06:10:18 +0000

    Mar 13, 2026, 11:40 AM

---
+++ Recutsion_IsSorted.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+isSorted(int *arr, size){
+    if(size == 0){
+        return false;
+    }
+
+}
+
+int main(){
+    arr[7] = {1,3,4,5,6,8,10};
+    isSorted(arr,7);
+    
+
+}
