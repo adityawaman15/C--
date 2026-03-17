c24b20f27fdbc5e892072d3cddebc0902651162a
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-03-17 16:10:41 +0000

    Mar 17, 2026, 9:40 PM

--- Recursion_BubbleSort.cpp
+++ Recursion_BubbleSort.cpp
@@ -2,21 +2,24 @@
 using namespace std;
 
 void sort(int *arr, int size){
-    if(size <= 1){
+    if(size <=1){
         return;
     }
 
-    if(arr[1]< arr[0]){
-        swap(arr[1],arr[0]);
+    for(int i = 0; i < size-1;i++){
+        if(arr[1]< arr[0]){
+            swap(arr[1],arr[0]);
+        }
     }
+    sort(arr,size-1);
 
-    sort(arr +1, size -1);
 }
 
 int main(){
-    int arr[10] = {1,5,6,7,9,2,34,34,32,13};
-    sort(arr,10);
-    for(int i:arr){
-        cout << arr[i]<< endl;
-    }
- }
+int arr[10] = {5,2,3,5,1,23,54,12,34,23};
+sort(arr,10);
+for(int i:arr){
+    cout << arr[i] << " ";
+}
+
+}
