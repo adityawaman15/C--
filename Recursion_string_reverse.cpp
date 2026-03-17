ac85361365e29e0be19f90901e23f4e85fed7af2
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-03-17 14:45:20 +0000

    Mar 17, 2026, 8:15 PM

---
+++ Recursion_string_reverse.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void reverse(string& str, int i,int j){
+
+    cout << "Calling for string | "<< str << endl;
+    if(i>=j){
+        return;
+    }
+
+    swap(str[i],str[j]);
+
+    reverse(str,i+1,j-1);
+    
+    
+}
+
+int main(){
+    string word = "Aditya";
+    reverse(word,0,word.length() -1 );
+    
+}
