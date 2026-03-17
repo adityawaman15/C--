9a3614b84881c73d5d938e4f60a485ea00ab3394
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-03-17 06:19:30 +0000

    Mar 17, 2026, 11:49 AM

--- Recursion_IsSorted.cpp
+++ Recursion_IsSorted.cpp
@@ -4,10 +4,16 @@ using namespace std;
 void isSorted(int *arr,int size){
 
     if(size == 0 || size == 1){
-        return true;
+        return;
     }
 
     cout << arr[0];
 
     isSorted(arr +1, size -1);
+}
+
+int main(){
+    int box[8] = {1,2,3,4,5,6,7,7};
+
+    isSorted(box,8);
 }
