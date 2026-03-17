7ffa126da0b2bfd9b531dc09f10e469cbf139625
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-03-17 06:42:27 +0000

    Mar 17, 2026, 12:12 PM

--- Recurstion_LinearSearch.cpp
+++ Recurstion_LinearSearch.cpp
@@ -8,6 +8,17 @@ bool search(int arr[], int size, int target){
         return 1;
     }
 
-        return search(arr + 1, size -1);
+        return search(arr + 1, size -1,target);
 
+}
+
+int main(){
+    int arr[7]={1,2,3,4,5,6,7};
+    bool ans = search(arr,7,9);
+    if(ans){
+        cout << "Target found";
+    }
+else {
+    cout << "Target not found";
+}
 }
