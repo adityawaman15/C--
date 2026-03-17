0dcc5243e54d35e6d175262c3533bc2ea81ca552
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-03-17 06:30:17 +0000

    Mar 17, 2026, 12:00 PM

--- Recursion_SumofElementsofArray.cpp
+++ Recursion_SumofElementsofArray.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int sum(int arr[], int size){
-    if(size == 0 || size == 1){
-        return arr[0];
+    if(size == 0){
+        return 0;
     }
 
     return arr[0] + sum(arr + 1, size -1);
