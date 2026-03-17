160591af51bc072f5c4f9e2297d47eba0dff3f71
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-03-17 14:36:38 +0000

    Mar 17, 2026, 8:06 PM

---
+++ Recursion_BinarySearch.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+bool Search(int arr[], int s,int e, int target){
+    if(s>e){
+        return false;
+    }
+
+    int mid = s = (e-s)/2;
+    if(arr[mid] == k){
+        return true;
+    }
+
+    if(arr[mid] < k){
+        return binarySearch(arr,mid+1,e,target);
+    }
+    else{
+        return binarySearch(arr,s,mid -1,k);
+    }
+}
+
+int main(){
+    int arr[11] = {2,4,6,20,24,28,38,49,55,222,231};
+    int size = 11;
+    int key = 28;
+
+    if(Search(arr,0,size-1,key)){
+        cout << "Yes";
+    }
+    else{
+        cout << "No";
+    }
+
+}
