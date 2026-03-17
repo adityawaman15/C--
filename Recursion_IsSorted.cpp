ce7aa403f376a6a132015b54207bb59de2e164ec
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-03-17 06:21:36 +0000

    Mar 17, 2026, 11:51 AM

--- Recursion_IsSorted.cpp
+++ Recursion_IsSorted.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
-using namespace std;
+                                        #include <iostream>
+                                        using namespace std;
 
-void isSorted(int *arr,int size){
+                                        void isSorted(int arr[],int size){
 
-    if(size == 0 || size == 1){
-        return;
-    }
+                                            if(size == 0 || size == 1){
+                                                return;
+                                            }
 
-    cout << arr[0];
+                                            cout << arr[0];
 
-    isSorted(arr +1, size -1);
-}
+                                            isSorted(arr +1, size -1);
+                                        }
 
-int main(){
-    int box[8] = {1,2,3,4,5,6,7,7};
+                                        int main(){
+                                            int box[8] = {1,2,3,4,5,6,7,7};
 
-    isSorted(box,8);
-}
+                                            isSorted(box,8);
+                                        }
