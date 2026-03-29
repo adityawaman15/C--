46b1b0cee5775e1eaa4b18ce46688803e66b57a5
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-03-29 13:23:34 +0000

    Mar 29, 2026, 6:53 PM

---
+++ Revision2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+string reverse(string &s){
+    int a = 0;
+    int b = s.length()-1;
+
+    while(a<=b){
+        swap(s[a],s[b]);
+    }
+}
+
+
+int main(){
+
+    string name = "Ich name ist Aditya.";
+    
+    name = reverse(name);
+
+    cout << name << endl;
+
+}
