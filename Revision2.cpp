de728298adbc0fa100619d34ff4d8bb4bb0a2ec9
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-03-31 07:04:43 +0000

    Mar 31, 2026, 12:34 PM

--- Revision2.cpp
+++ Revision2.cpp
@@ -6,11 +6,13 @@ using namespace std;
 int main(){
 
 int a = 1;
-int i = a++;
-vector<vector<int>> matrix(3,vector<int>(3,i));
+int i = 1;
+ i = i++;
+vector<vector<int>> matrix(3,vector<int>(3,i++));
 
-for(int i: matrix){
-    cout << i << endl;
+for(int i = 0; i < matrix.size()* matrix[0].size(); i++){
+
+    cout << matrix[i/3][i%3] << " ";
 }
 
 
