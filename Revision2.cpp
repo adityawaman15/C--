8b2feb2aa0d7b4d6a0466ed1f8318dc96dd84a6d
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-03-31 08:17:03 +0000

    Mar 31, 2026, 1:47 PM

--- Revision2.cpp
+++ Revision2.cpp
@@ -1,41 +1,39 @@
-#include <iostream>
-#include <vector>
 
+#include <iostream>
 using namespace std;
 
 
-
-void zero(int r, int c, vector<vector<int>>& matrix){
-    int cols = 4;
-    int rows = 4;
-
-    for(int i = 0; i< cols; i++){
-        matrix[i][r-1] = 0;
+    void zero(int r, int c, vector<vector<int>>& matrix){
+    
+    for(int i = 0; i< matrix[0].size(); i++){
+        matrix[i][r] = 0;
     }
-    for(int i = 0; i <rows;i++){
-        matrix[c-1][i] = 0;
+    for(int i = 0; i <matrix.size();i++){
+        matrix[c][i] = 0;
     }
 
 
 }
 int main(){
-int num = 1;
-vector<vector<int>> matrix(4,vector<int>(4,4));
 
-for(int i = 0; i<matrix.size(); i++){
-    for(int j = 0; j < matrix[0].size();j++){
-        matrix[i][j] = num++;
+    vector<vector<int>> matrix = {{1,1,1},{1,0,1}{1,1,1}};
 
+        vector<vector<int>> coords;
+
+        for(int i = 0; i < matrix.size();i++){
+            for(int j = 0; j < matrix[0].size(); j++){
+                if(matrix[i][j] == 0){
+                    coords.push_back({i,j});
+                }
+            }
+        }
+
+        for(int i = 0; i < coords.size(); i++){
+            zero(coords[i][0],coords[i][1],matrix);
+        }
+
+    
+
+        
     }
-}
-
-zero(2,3,matrix);
-
-for(int i = 0; i < 16; i++){
-
-    cout << matrix[i/4][i%4] << " ";
-    if( (i+1)%4 == 0){
-        cout << endl;
-    }
-
-}}
+   
