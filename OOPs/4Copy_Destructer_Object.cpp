ede2244c29420351835cc2d2612ec599b35e1dc8
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-05-01 12:10:49 +0000

    May 1, 2026, 5:40 PM

---
+++ OOPs/4Copy_Destructer_Object.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Hero{
+    public:
+    int health;
+    int lvl;
+    string name;
+
+    Hero(int health, int lvl, string name){
+        this->health = health;
+        this->lvl = lvl;
+        this->name=name;
+    }
+
+    print(){
+        cout << "Player Details-----" << endl;
+        cout << "Health = " << health<< endl;
+        cout << "Level = " << lvl << endl;
+        cout << "Name = " << name << endl;
+        
+    }
+
+};
+
+int main(){
+    Hero Aditya(1242,12,"Aditya");
+    Aditya.print();
+
+    Hero Saish = Aditya;
+    Saish.print();
+
+}
