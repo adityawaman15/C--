dad88b9402287705a0fb903f6caf6c9bd32e14b1
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-05-23 13:49:31 +0000

    May 23, 2026, 7:19 PM

---
+++ LInked List/Revision.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+     int data;
+     Node* next;
+
+     Node(int d){
+        this->data = d;
+        this->next = NULL;
+     }
+};
+
+void head(){
+
+}
+int main(){
+    
+}
