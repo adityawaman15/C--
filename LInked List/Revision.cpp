71356349d0d73256aad471b043acd11524fdadcc
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-05-23 14:03:08 +0000

    May 23, 2026, 7:33 PM

--- LInked List/Revision.cpp
+++ LInked List/Revision.cpp
@@ -12,9 +12,36 @@ class Node{
      }
 };
 
-void head(){
+void InsertAtHead(Node* &head, int d){
+    Node* temp = new Node(d);
+    temp -> next = head;
+    head = temp;
 
 }
+void InsertAtTail(Node* &tail, int d){
+    Node* temp = new Node(d);
+    tail -> next = temp;
+    tail = temp;
+
+}
+
+void print(Node* head){
+ Node* temp = head;
+ while(temp->level != NULL){
+    cout << temp->data << endl;
+    temp = temp -> next;
+ }
+}
 int main(){
+
+    Node* Node1 = new Node(10);
+
+    Node* head = Node1;
+    Node* tail = Node1;
+    InsertAtHead(head,5);
+    InsertAtTail(tail,15);
+
+    print(head);
     
 }
+
