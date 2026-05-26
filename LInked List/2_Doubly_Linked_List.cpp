8f31fabefeb4df3223b907f14b7577ef24852c47
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-05-26 14:48:33 +0000

    May 26, 2026, 8:18 PM

--- LInked List/2_Doubly_Linked_List.cpp
+++ LInked List/2_Doubly_Linked_List.cpp
@@ -30,6 +30,47 @@ int insertAtHead(Node* &head, int d){
     head-> prev = temp;
     head = temp;
 
+
+}
+
+void InsertAtTail(Node* &tail, int d){
+    Node* temp = new Node(d);
+    temp-> prev = tail;
+    tail->next = temp;
+    tail = temp;
+}
+
+void InsertNode(Node* &head, Node* &tail, int position, int d){
+
+    Node* temp = head;
+    Node* temp1;
+
+    if(position == 0){
+        insertAtHead(head,d);
+        return;
+    }
+    
+    int cnt = 0;
+    while(cnt < position-1){
+ 
+        temp = temp -> next;
+        cnt++;
+    }
+
+    if(temp->next == NULL){
+        InsertAtTail(tail,d);
+        return;
+
+    }
+    temp1= temp->next;
+
+    Node* NodetobeInserted = new Node(d);
+    NodetobeInserted->next = temp ->next;
+    temp-> next = NodetobeInserted;
+    NodetobeInserted-> prev = temp;
+    temp1->prev = NodetobeInserted;
+
+
 }
 
 int main(){
