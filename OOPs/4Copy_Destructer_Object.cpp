8e73e9c5c413b9b9cc0115bcba31b44d822136b0
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-05-26 13:30:37 +0000

    May 26, 2026, 7:00 PM

--- OOPs/4Copy_Destructer_Object.cpp
+++ OOPs/4Copy_Destructer_Object.cpp
@@ -4,39 +4,47 @@ using namespace std;
 class Hero{
     public:
     int health;
-    int *lvl;
-    string *name;
+    int lvl;
+    string name;
+    static int Timetocomplete;
+    //It contains a single memory throught all objects in a class;
+    //You dont need to create object to access it.
+    //You can make static functions but only have acces to static datatypes you created;
 
     Hero(int health, int lvl, string name){
+        cout << "Constructor called" << endl;
+
         this->health = health;
         this->lvl = lvl;
-        this->name=name;
+        this->name = name;
     }
-    
-     
 
-    
-
-    print(){
+    void print(){
         cout << "Player Details-----" << endl;
-        cout << "Health = " << health<< endl;
+        cout << "Health = " << health << endl;
         cout << "Level = " << lvl << endl;
         cout << "Name = " << name << endl;
-        
     }
 
+    ~Hero(){
+        cout << "Destructor called" << endl;
+    }
 };
 
+int Hero::Timetocomplete=5;
+
 int main(){
+
     Hero Aditya(1242,12,"Aditya");
     Aditya.print();
+    Aditya.Timetocomplete = 10;
+ //Destructor is called automatically in Static.
+    Hero* Saish = new Hero(123,4,"Saish");
+    //value of both changes because timetocomplete is static and holds same memory;
+    cout << Aditya.Timetocomplete << " and " << Saish->Timetocomplete<< endl;
 
-    Hero Saish(Aditya);
 
-    Aditya.lvl = 124;
-    Saish.print();
-    
+    delete Saish;
 
-    //So we make a variable inside the function;
-
+    return 0;
 }
---
+++ OOPs/5OOPs_Properties.cpp
@@ -0,0 +1,1 @@
+//https://www.naukri.com/code360/guided-paths/basics-of-c/content/118817/offering/1382190
