99fc645da0f59f9b44acf32123e89b8963cb6baa
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-06-11 12:53:12 +0000

    Jun 11, 2026, 6:23 PM

--- LInked List/Revision.cpp
+++ LInked List/Revision.cpp
@@ -6,36 +6,72 @@ class Node{
     int data;
     Node* next;
 
-    Node(int data){
-        this-> data = data;
-        this-> next = NULL;
+    Node(int d){
+        data = d;
+        next = NULL;
+    }
+
+    ~Node(){
+        next = NULL;
+        if(this->next!= NULL){
+            delete next;
+            this->next;
+        }
     }
 };
 
-void InsertAtHead(Node* &heads, int d){
+InsertAtHead(int d, Node* &head){
     Node* temp = new Node(d);
-    temp->next = heads;
-    heads = temp;
+    temp->next = head;
+    head = temp;
+
 }
 
-void InsertAtTail(Node* &tails, int d){
+InsertAtTail(int d, Node* &tail){
     Node* temp = new Node(d);
-    tails->next = temp;
-    tails = temp;
+    tail-> next = temp;
+    tail = temp;
 }
 
-void Print(Node* &heads){
-    Node* temp = heads;
-    while (temp != NULL){
-        cout << temp->data << endl;
+InsertAtIndex(int d, int index,  Node* &head, Node* &tail){
+    Node* temp = head;
+    int cnt = 0;
+
+    if(index == 0){
+        InsertAtHead(d, head);
+        return;
+    }
+    
+    while(cnt < index -1){
+        temp = temp->next;
+        cnt++;
+    }
+    if(temp->next == NULL){
+        InsertAtTail(d,tail);
+        return;
+    }
+
+    Node* NodeToBeInserted = new Node(d);
+    NodeToBeInserted-> next = temp-> next;
+    temp->next = NodeToBeInserted;
+    
+    
+}
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout << temp->data << " ";
         temp = temp->next;
     }
-}
-int main(){
-    Node* Node1 = new Node(15);
-    
-    Node* heads = Node1;
-    Node* tails = Node1;
 
+
+}
+
+int main(){
+
+    Node* Node1 = new Node(10);
+    Node* head = Node1;
+    Node* tail = Node1;
 
 }
