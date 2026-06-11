5483003d0657599fabef9e1d38ff605b7c6e8c49
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-06-11 13:02:30 +0000

    Jun 11, 2026, 6:32 PM

--- LInked List/Revision.cpp
+++ LInked List/Revision.cpp
@@ -5,73 +5,40 @@ class Node{
     public:
     int data;
     Node* next;
+    Node* prev;
 
     Node(int d){
-        data = d;
-        next = NULL;
+        this->data = d;
+        this->next = NULL;
+        this->prev = NULL;
     }
 
     ~Node(){
-        next = NULL;
-        if(this->next!= NULL){
+        int value = this->data;
+        if(next != NULL){
             delete next;
-            this->next;
+            next = NULL;
         }
+        cout << "memory free of node with data"<< value << endl;
     }
-};
-
-InsertAtHead(int d, Node* &head){
-    Node* temp = new Node(d);
-    temp->next = head;
-    head = temp;
-
 }
-
-InsertAtTail(int d, Node* &tail){
-    Node* temp = new Node(d);
-    tail-> next = temp;
-    tail = temp;
-}
-
-InsertAtIndex(int d, int index,  Node* &head, Node* &tail){
-    Node* temp = head;
-    int cnt = 0;
-
-    if(index == 0){
-        InsertAtHead(d, head);
-        return;
-    }
-    
-    while(cnt < index -1){
-        temp = temp->next;
-        cnt++;
-    }
-    if(temp->next == NULL){
-        InsertAtTail(d,tail);
-        return;
-    }
-
-    Node* NodeToBeInserted = new Node(d);
-    NodeToBeInserted-> next = temp-> next;
-    temp->next = NodeToBeInserted;
-    
-    
-}
+;
 
 void print(Node* &head){
     Node* temp = head;
     while(temp != NULL){
         cout << temp->data << " ";
         temp = temp->next;
+
     }
-
-
+    cout << endl;
 }
 
 int main(){
-
     Node* Node1 = new Node(10);
     Node* head = Node1;
     Node* tail = Node1;
+
+    print(head);
 
 }
--- LInked List/tempCodeRunnerFile.cpp
+++ LInked List/tempCodeRunnerFile.cpp
@@ -1,1 +1,44 @@
-        deleteNode(head,tail,3);
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int d){
+        this->data = d;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+
+    ~Node(){
+        int value = this->data;
+        if(next != NULL){
+            delete next;
+            next = NULL;
+        }
+        cout << "memory free of node with data"<< value << endl;
+    }
+}
+;
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout << temp->data << " ";
+        temp = temp->next;
+
+    }
+    cout << endl;
+}
+
+int main(){
+    Node* Node1 = new Node(10);
+    Node* head = Node1;
+    Node* tail = Node1;
+
+    print(head);
+
+}
