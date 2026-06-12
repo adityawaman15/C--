d9fb00470e41c3e9fd3c113637f77fa7941e026b
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-06-12 08:39:46 +0000

    Jun 12, 2026, 2:09 PM

--- LInked List/Revision.cpp
+++ LInked List/Revision.cpp
@@ -1,44 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class Node{
-    public:
-    int data;
-    Node* next;
-    Node* prev;
-
-    Node(int d){
-        this->data = d;
-        this->next = NULL;
-        this->prev = NULL;
-    }
-
-    ~Node(){
-        int value = this->data;
-        if(next != NULL){
-            delete next;
-            next = NULL;
-        }
-        cout << "memory free of node with data"<< value << endl;
-    }
-}
-;
-
-void print(Node* &head){
-    Node* temp = head;
-    while(temp != NULL){
-        cout << temp->data << " ";
-        temp = temp->next;
-
-    }
-    cout << endl;
-}
-
 int main(){
-    Node* Node1 = new Node(10);
-    Node* head = Node1;
-    Node* tail = Node1;
-
-    print(head);
-
+    int a =4;
+    cout << a++ << a << ++a << ++a << a++ << a << endl;
 }
