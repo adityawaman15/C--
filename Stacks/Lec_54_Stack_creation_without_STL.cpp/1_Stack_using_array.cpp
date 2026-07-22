ec6fdad93b98a5aca86496dc82741c8a86a94460
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-07-22 12:57:38 +0000

    Jul 22, 2026, 6:27 PM

--- Stacks/Lec_54_Stack_creation_without_STL.cpp/1_Stack_using_array.cpp
+++ Stacks/Lec_54_Stack_creation_without_STL.cpp/1_Stack_using_array.cpp
@@ -1,44 +1,58 @@
 #include <iostream>
 using namespace std;
 
-class Stack{
-
-    public: 
-
+class Stack {
+public:
     int* arr;
     int top;
     int size;
-    
-    Stack(){
+
+    Stack(int s) {
+        size = s;
         arr = new int[size];
         top = -1;
     }
 
-    void push(int element){
-        if(size-top> 1){
-            arr[top++] = element;
-        }
-        else{
-            cout << "Stack OVERFLOWWW !! HABIBI" << endl;         
+    void push(int element) {
+        if (top < size - 1) {
+            arr[++top] = element;
+        } else {
+            cout << "Stack Overflow!" << endl;
         }
     }
 
-    void top{
-        if(top>=0){
+    void pop() {
+        if (top >= 0) {
+            top--;
+        } else {
+            cout << "Stack Underflow!" << endl;
+        }
+    }
+
+    void peek() {
+        if (top >= 0) {
             cout << "Top element is " << arr[top] << endl;
-        }
-        else{
-            cout << "The Stack is empty" << endl;
-        }
-    }
-
-    void pop(){
-        if(top>=0){
-            arr[top--] = NULL;
-        }
-        else{
-            cout << "Stack is empty HABIBI (Stack Underflow), What you on!? " << endl;
+        } else {
+            cout << "Stack is empty." << endl;
         }
     }
 
+    bool isEmpty() {
+        return top == -1;
+    }
+};
+
+int main() {
+    Stack st(5);
+
+    st.push(10);
+    st.push(20);
+    st.push(30);
+
+    st.peek();
+
+    st.pop();
+    st.peek();
+
+    return 0;
 }
