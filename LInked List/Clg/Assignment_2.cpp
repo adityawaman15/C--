cf54ea2d0b4eb16f2bdd5c9fd7af808a226a23d2
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-07-27 09:46:43 +0000

    Jul 27, 2026, 3:16 PM

--- LInked List/Clg/Assignment_2.cpp
+++ LInked List/Clg/Assignment_2.cpp
@@ -1,61 +1,69 @@
 #include <iostream>
 using namespace std;
 
-void Bubble_Sort( int arr[]){
-    int size = sizeof(arr)/4;
-    for(int i = 0; i <size-1; i++){
-        bool swap = false;
-        for(int j = 0; i <size -j-1; j++){
-            if(arr[j+1]<arr[j]){
+void Bubble_Sort(int arr[], int size)
+{
+    for(int i = 0; i < size - 1; i++)
+    {
+        bool swapped = false;
+
+        for(int j = 0; j < size - i - 1; j++)
+        {
+            if(arr[j] > arr[j + 1])
+            {
                 int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
             }
-
         }
+
+        if(!swapped)
+            break;
     }
-
-
 }
 
-void print(int arr[]){
-    int size = sizeof(arr)/4;
-    for(int i = 0; i <size ; i++){
+void print(int arr[], int size)
+{
+    for(int i = 0; i < size; i++)
         cout << arr[i] << " ";
-    }
+
+    cout << endl;
 }
 
-int main() {
-    //Bubble sort
+int main()
+{
     int arr[10] = {34,654,234,235,65,34,23,65,76,10};
+
     char flag;
     int num;
 
-
-    do {
-        cout << "Enter :-" << endl; 
-        cout << "1 | To print the array " << endl;
-        cout << "2 | To sort the array " << endl;
+    do
+    {
+        cout << "\nEnter:-" << endl;
+        cout << "1 | Print Array" << endl;
+        cout << "2 | Sort Array" << endl;
         cin >> num;
 
-        switch(num){
-            case (1):
-             print();
-            case (2):
-             Bubble_Sort();
+        switch(num)
+        {
+            case 1:
+                print(arr, 10);
+                break;
+
+            case 2:
+                Bubble_Sort(arr, 10);
+                cout << "Array Sorted!" << endl;
+                break;
+
             default:
-             break;
+                cout << "Invalid Choice!" << endl;
         }
 
-        cout << "Do you want to continue 'Y or N |' " << endl;
+        cout << "Do you want to continue (Y/N)? ";
         cin >> flag;
 
+    } while(flag == 'Y' || flag == 'y');
 
-        
-    }
-    while(flag!= 'Y');
-
-
-
-
+    return 0;
 }
