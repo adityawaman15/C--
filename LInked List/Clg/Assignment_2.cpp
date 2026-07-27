d4e7c010f29a74eea8f4c5872e0f4130815fa75e
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-07-27 09:48:28 +0000

    Jul 27, 2026, 3:18 PM

--- LInked List/Clg/Assignment_2.cpp
+++ LInked List/Clg/Assignment_2.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 void Bubble_Sort(int arr[], int size)
-{
+{   
+    int count = 0;
     for(int i = 0; i < size - 1; i++)
     {
         bool swapped = false;
+        
+
 
         for(int j = 0; j < size - i - 1; j++)
         {
@@ -15,12 +18,15 @@ void Bubble_Sort(int arr[], int size)
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
                 swapped = true;
+                count++;
             }
         }
 
         if(!swapped)
             break;
     }
+    
+    cout << "Number of times swapped = " << count << endl;
 }
 
 void print(int arr[], int size)
