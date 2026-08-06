f437b20efbff520f5b99328d89c10fdc372dca09
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-08-06 09:58:16 +0000

    Aug 6, 2026, 3:28 PM

---
+++ LInked List/Clg/LL_creation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+int main(){
+    int ch;
+
+    do{
+        cout << "\n *****SLL Menu*****\n";
+        cout << "1) Add Node at First\n";
+        cout << "2) Add Node at Last\n";
+        cout << "3) Add Node in Between\n";
+        cout << "4) Display\n";
+        cout << "5) Delete\n";
+
+        cout << "7) Exit\n\n";
+        cout << "Enter your choice | ";
+        cin >> ch;
+
+        switch (ch)
+        {
+            case 1:
+              break;
+            case 2:
+              break;
+            case 3:
+              break;
+            case 4:
+              break;
+            default:
+                exit(0);
+            
+        }
+    }while(ch!= 7);
+}
