f451753bd30f0b1ba4fc5393d40098c3fb634a78
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-09-13 07:48:36 +0000

    Sep 13, 2026, 1:18 PM

--- CP/3_floating_numbers.cpp
+++ CP/3_floating_numbers.cpp
@@ -5,16 +5,22 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    typedef double d;
 
-    double x = 0.3*3;
 
-    double y = 0.9;
+    d x = 0.3*3;
+
+    d y = 0.9;
     /*A difficulty when using floating point numbers is that some numbers cannot
 be represented accurately as floating point numbers, and there will be rounding
 errors. For example, the result of the following code is surprising:
 */
     printf("%.20f \n", x);
     printf("%.20f \n", y);
+
+    //abs = absolute value
+    //1e-9 = 1* 10^-9
+    //3e-19 = 3 & 10^-19
 
     if(abs(x-y) < 1e-9){
         cout << "x and y are same";
