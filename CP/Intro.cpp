a85af45d3b49c9d9c2c6195a5f42521141b5971f
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-09-13 06:18:19 +0000

    Sep 13, 2026, 11:48 AM

---
+++ CP/Intro.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+/*
+The #include line at the beginning of the code is a feature of the g++ compiler
+that allows us to include the entire standard library. Thus, it is not needed to
+separately include libraries such as iostream, vector and algorithm, but rather
+they are available automatically.
+*/
+using namespace std;
+
+int main(){
+    cout << "Hello, World!" << endl;
+}
+
