54bb5c7e9c920bbc99d194ae3be192a0fe204bab
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-09-14 17:16:42 +0000

    Sep 14, 2026, 10:46 PM

--- Trees/62_Binary_Tree_and_Its_Representation.cpp
+++ Trees/62_Binary_Tree_and_Its_Representation.cpp
@@ -66,6 +66,40 @@ void LevelOrderTraversal(node* root){
     }
 }
 
+void Inorder(node* root){
+    if(root == NULL){
+        return;
+    }
+
+    Inorder(root->left);
+    cout << root->data << " ";
+    Inorder(root->right);
+}
+
+void preorder(node* root) {
+    //base case
+    if(root == NULL) {
+        return ;
+    }
+
+    cout << root-> data << " ";
+    preorder(root->left);
+    preorder(root->right);
+
+}
+
+void postorder(node* root) {
+    //base case
+    if(root == NULL) {
+        return ;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root-> data << " ";
+
+}
+
 
 int main(){
 
