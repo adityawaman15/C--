56767760c20cb49ac7bb15acca9dde5978504b92
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-09-15 09:11:55 +0000

    Sep 15, 2026, 2:41 PM

--- Trees/62_Binary_Tree_and_Its_Representation.cpp
+++ Trees/62_Binary_Tree_and_Its_Representation.cpp
@@ -65,6 +65,39 @@ void LevelOrderTraversal(node* root){
     }
     }
 }
+void buildFromLevelOrder(node* &root) {
+    queue<node*> q;
+
+    cout << "Enter data for root" << endl;
+    int data ;
+    cin >> data;
+    root = new node(data);
+    
+    q.push(root);
+
+    while(!q.empty()) {
+        node* temp = q.front();
+        q.pop();
+
+        cout << "Enter left node for: " << temp->data << endl;
+        int leftData;
+        cin >> leftData;
+
+        if(leftData != -1) {
+            temp -> left = new node(leftData);
+            q.push(temp->left);
+        }
+
+        cout << "Enter right node for: " << temp->data << endl;
+        int rightData;
+        cin >> rightData;
+
+        if(rightData != -1) {
+            temp -> right = new node(rightData);
+            q.push(temp->right);
+        }
+    }
+ }  
 
 void Inorder(node* root){
     if(root == NULL){
---
+++ Trees/tp.cpp
@@ -0,0 +1,27 @@
+/*
+Node* LevelOrderTraverse(Node* Root){   
+    queue <Node*> q;
+    q.push(Root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        cout << temp->data << " ";
+        q.pop(); 
+        
+        if(temp == NULL)){
+            cout << "\n";
+            if(!q.empty()){
+             q.push(NULL);}
+        }}
+
+         if(temp->left){
+            q.push(temp->left);
+        }
+
+        if(temp->right){
+            q.push(temp->right);
+        }
+
+}
+*/
