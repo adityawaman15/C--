ec3e0d8733b9366977c545670ba29653f509748e
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-09-25 16:06:51 +0000

    Sep 25, 2026, 9:36 PM

--- CP/codeforces contest/25 sept/Fashionable_array.cpp
+++ CP/codeforces contest/25 sept/Fashionable_array.cpp
@@ -24,22 +24,31 @@ int main() {
         for(int i = 0; i<n;i++){
             cin >> arr[i];
             count[arr[i]]++;
-            if(count[arr[i]] > max_count){
-                max_count++;
-            }
+
         }
 
        vector<vector<int>> ans;
 
+
+       for(auto k:count){
+        int max_count = k.second;
+        for(int l = 0; l < max_count;l++){
+            cout << k.first << " ";
+            k.second--;
+        }
+        
        for(int i = 0; i <max_count;i++){
-        int curr_count = i+1;
+        int curr_count = max_count;
         for(auto j:count){
             if(j.second >= curr_count){
                 cout << j.first << " ";
+                j.second--;
             }
         }
 
+
        }
+    }
 
 
     }
