cae354c645644eac79da035cfe9046464cca7340
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-09-25 14:17:53 +0000

    Sep 25, 2026, 7:47 PM

---
+++ Trees/Binary Search Tree/1_BST_introduction.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        Node* left;
+        Node* right;
+        int data;
+    
+    Node(int d){
+        this->left = NULL;
+        this->right = NULL;
+        this->data = d;
+    }
+};
+
+void LevelOrderTraversal(Node* root){
+    queue <Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+
+        Node* temp = q.front();
+        
+        q.pop();
+
+
+        if(temp == NULL){ //purana level pura traverse ho chuka he
+            cout << endl;
+            if(!q.empty()){ //queue still has some child nodes
+                q.push(NULL);
+
+            }
+        }
+        else{
+        cout << temp->data << " ";
+        if(temp->left){
+            q.push(temp->left);
+        }
+
+        if(temp->right){
+            q.push(temp->right);
+        }
+    }
+    }
+}
+
+void InsertIntoBst(Node* &root,int data){
+    if(root == NULL){
+        root = new Node(data);
+    }
+    else{
+       if(root->data < data){
+        InsertIntoBst(root->right, data);
+       }
+       else{
+        InsertIntoBst(root->left, data);
+       }
+    }
+}
+
+void takeInput(Node* &root){
+    int data;
+    cout << "Enter data | ";
+    cin >> data;
+
+    while(data != -1){
+        InsertIntoBst(root,data);
+        cin >> data;
+    }
+}
+
+int main(){
+
+    Node* root = NULL;
+    takeInput(root);
+
+    LevelOrderTraversal(root);
+
+
+}
